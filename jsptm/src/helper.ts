9efb5efd22530f425168bcf9fa0a581c9e542fb7
Puellaquae <shentukeqin@hotmail.com>
2022-03-20 16:51:20 +0800

    better type for easyMap

--- jsptm/src/helper.ts
+++ jsptm/src/helper.ts
@@ -1,19 +1,21 @@
-function easyMap<K>(map: Map<K, unknown> = new Map()) {
+function easyMap<M>(map: Map<string, unknown> = new Map()) {
+    type Filter<T, U> = T extends U ? T : never;
+    type Keys = Filter<keyof M, string>;
     return {
         map,
-        get: (key: K) => map.get(key),
-        set: (key: K, value: unknown) => map.set(key, value),
-        has: (key: K) => map.has(key),
-        forEach: (callbackfn: (value: unknown, key: K, map: Map<K, unknown>) => void, thisArg?: any) => map.forEach(callbackfn, thisArg),
+        get<K extends Keys, V = M[K]>(key: K): V { return map.get(key) as V },
+        set<K extends Keys, V = M[K]>(key: K, value: V) { map.set(key, value) },
+        has: (key: string) => map.has(key),
+        forEach: (callbackfn: (value: unknown, key: string, map: Map<string, unknown>) => void, thisArg?: any) => map.forEach(callbackfn, thisArg),
         values: () => map.values(),
         keys: () => map.keys(),
         entries: () => map.entries(),
         size: () => map.size,
         clear: () => map.clear(),
-        delete: (key: K) => map.delete(key),
-        entry<V>(prop: K) {
+        delete<K extends Keys>(key: K) { map.delete(key) },
+        entry<K extends Keys, V = M[K]>(prop: K) {
             return {
-                __mapRef: this.map as Map<K, unknown>,
+                __mapRef: this.map as Map<string, unknown>,
                 or(val: V) {
                     if (!this.__mapRef.has(prop)) {
                         this.__mapRef.set(prop, val);
--- jsptm/test/helper.test.ts
+++ jsptm/test/helper.test.ts
@@ -1,22 +1,22 @@
 import { easyMap } from "../src/helper";
 
 it("easyMap", () => {
-    let m = easyMap();
-    m.entry<string>("yy").val = "12";
+    let m = easyMap<{ yy: string, zz: number }>();
+    m.entry("yy").val = "12";
     expect(m.get("yy")).toBe("12");
-    m.entry<string>("yy").or("12").val = "21";
+    m.entry("yy").or("12").val = "21";
     expect(m.get("yy")).toBe("21");
-    m.entry<number>("zz").or(1).val += 2;
+    m.entry("zz").or(1).val += 2;
     expect(m.get("zz")).toBe(3);
 });
 
 it("nonCopy", () => {
     let rm = new Map();
-    let m = easyMap(rm);
-    m.entry<string>("yy").val = "12";
+    let m = easyMap<{ yy: string, zz: number }>(rm);
+    m.entry("yy").val = "12";
     expect(rm.get("yy")).toBe("12");
-    m.entry<string>("yy").or("12").val = "21";
+    m.entry("yy").or("12").val = "21";
     expect(rm.get("yy")).toBe("21");
-    m.entry<number>("zz").or(1).val += 2;
+    m.entry("zz").or(1).val += 2;
     expect(rm.get("zz")).toBe(3);
 });
